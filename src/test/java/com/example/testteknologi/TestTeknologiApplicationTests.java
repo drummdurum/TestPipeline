9b30303cf382a715c42d4cbb0382b08d765f78dd
drummdurum <147395124+drummdurum@users.noreply.github.com>
2025-01-08 12:02:41 +0100

    start commit

---
+++ src/test/java/com/example/testteknologi/TestTeknologiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testteknologi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTeknologiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
